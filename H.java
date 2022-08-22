f342db64c055b646996d9c5d2a1fdcac6d85cb0d
balajiudy-github <84365865+balajiudy-github@users.noreply.github.com>
2022-08-22 18:17:43 +0530

    tic#111
    111

--- H.java
+++ H.java
@@ -3,7 +3,8 @@ public class H {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		int a = 20;
+		System.out.println(a*a);
 	}
 
 }
